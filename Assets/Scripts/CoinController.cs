8c11dcfd1f57efa2609a357d90791b0a6b661668
CYBERZERO\cyberzero <karthi5300@ymail.com>
2022-08-24 19:34:48 +0530

    Coin Modelling and Animation

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -1,6 +1,32 @@
+using System.Collections;
 using UnityEngine;
 
 public class CoinController : MonoBehaviour
 {
+    //var to set the coin rotation spedd in Inspector
+    [SerializeField]
+    //1.0f rotattions/sec
+    private float rotateSpeed = 1f;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        //starts the Coroutine
+        StartCoroutine(Spin());
+    }
+    //to spin the coin.All Coroutines must have a return Type IEnumerator
+    private IEnumerator Spin()
+    {
+        //to rotate smootly a bit on each frame
+        while (true)
+        {
+            //transform.Rotate => transform function from Rotate class
+            //transform.up => along y axis
+            //Time.deltaTime => how much time since last frame
+            //360 * rotateSpeed * Time.deltaTime => how much i nedd to rotate each frame
+            transform.Rotate(transform.up, 90 * rotateSpeed * Time.deltaTime);
+            // to avoid eternal loop
+            yield return 0;
+        }
+    }
 }
