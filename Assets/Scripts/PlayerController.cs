835e9d094a69a2839b5cc9ebe05662b1fa2160cd
CYBERZERO\cyberzero <karthi5300@ymail.com>
2022-08-25 20:40:08 +0530

    Restricted Player movement outside platform

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,10 @@ public class PlayerController : MonoBehaviour
 
     private bool m_isMoving;
     private int m_playerMoveCount = 10;
+    private bool m_moveLeft;
+    private bool m_moveRight;
+    private bool m_moveForward;
+    private bool m_moveBack;
 
     public System.Action OnPlayerMove;
     public System.Action OnPlayerCollectCoin;
@@ -21,25 +25,33 @@ public class PlayerController : MonoBehaviour
         if (m_isMoving)
             return;
 
-        if (Input.GetKeyDown(KeyCode.A)) Assemble(Vector3.left);
-        else if (Input.GetKeyDown(KeyCode.D)) Assemble(Vector3.right);
-        else if (Input.GetKeyDown(KeyCode.W)) Assemble(Vector3.forward);
-        else if (Input.GetKeyDown(KeyCode.S)) Assemble(Vector3.back);
+        if (m_moveLeft) { if (Input.GetKeyDown(KeyCode.A)) Assemble(Vector3.left); }
 
-        void Assemble(Vector3 dir)
-        {
-            var anchor = transform.position + (Vector3.down + dir) * 0.5f;
-            var axis = Vector3.Cross(Vector3.up, dir);
-            StartCoroutine(Roll(anchor, axis));
+        if (m_moveRight) { if (Input.GetKeyDown(KeyCode.D)) Assemble(Vector3.right); }
 
-            if (OnPlayerMove != null)
-                OnPlayerMove();
+        if (m_moveForward) { if (Input.GetKeyDown(KeyCode.W)) Assemble(Vector3.forward); }
 
-            if (--m_playerMoveCount == 0)
-                if (OnPlayerRunOutOfMoves != null)
-                    OnPlayerRunOutOfMoves();
-        }
+        if (m_moveBack) { if (Input.GetKeyDown(KeyCode.S)) Assemble(Vector3.back); }
+
+        transform.rotation = Quaternion.identity;
+
+        CheckBorder();
     }
+
+    void Assemble(Vector3 dir)
+    {
+        var anchor = transform.position + (Vector3.down + dir) * 0.5f;
+        var axis = Vector3.Cross(Vector3.up, dir);
+        StartCoroutine(Roll(anchor, axis));
+
+        if (OnPlayerMove != null)
+            OnPlayerMove();
+
+        if (--m_playerMoveCount == 0)
+            if (OnPlayerRunOutOfMoves != null)
+                OnPlayerRunOutOfMoves();
+    }
+
 
     IEnumerator Roll(Vector3 anchor, Vector3 axis)
     {
@@ -70,5 +82,52 @@ public class PlayerController : MonoBehaviour
             if (OnPlayerCompleteLevel != null)
                 OnPlayerCompleteLevel();
         }
+
+    }
+
+    void CheckBorder()
+    {
+        //creating the ray
+        Ray forwardRay = new Ray(transform.position, Vector3.forward);
+        Ray backRay = new Ray(transform.position, Vector3.back);
+        Ray leftRay = new Ray(transform.position, Vector3.left);
+        Ray rightRay = new Ray(transform.position, Vector3.right);
+
+        //creating variable for object that gets hit by the ray
+        RaycastHit hitInfo;
+
+        //Raycast function to check for the ray being hit
+        if (Physics.Raycast(leftRay, out hitInfo, 1))
+        {
+            if (hitInfo.collider.gameObject.CompareTag("Wall"))
+                m_moveLeft = false;
+        }
+        else
+            m_moveLeft = true;
+
+        if (Physics.Raycast(rightRay, out hitInfo, 1))
+        {
+            if (hitInfo.collider.gameObject.CompareTag("Wall"))
+                m_moveRight = false;
+        }
+        else
+            m_moveRight = true;
+
+        if (Physics.Raycast(forwardRay, out hitInfo, 1))
+        {
+            if (hitInfo.collider.gameObject.CompareTag("Wall"))
+                m_moveForward = false;
+        }
+        else
+            m_moveForward = true;
+
+        if (Physics.Raycast(backRay, out hitInfo, 1))
+        {
+            if (hitInfo.collider.gameObject.CompareTag("Wall"))
+                m_moveBack = false;
+        }
+        else
+            m_moveBack = true;
+
     }
 }
